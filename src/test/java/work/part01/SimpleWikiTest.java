5dd066fcac85ca48264b07737d14c502b9c983a9
bytyavka <natgenyur@mail.ru>
2025-12-06 19:09:54 +0300

    DZ1

--- src/test/java/work/part01/SimpleWikiTest.java
+++ src/test/java/work/part01/SimpleWikiTest.java
@@ -1,11 +1,13 @@
-package demo.part01;
+package work.part01;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+
 public class SimpleWikiTest {
     @BeforeAll
     static void beforeAll() {
