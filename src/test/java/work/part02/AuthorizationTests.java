68c642927644e85e119a4bba650de0283d2f9f52
bytyavka <natgenyur@mail.ru>
2025-12-07 15:49:41 +0300

    DZ3

--- src/test/java/work/part02/AuthorizationTests.java
+++ src/test/java/work/part02/AuthorizationTests.java
@@ -19,10 +19,10 @@ public class AuthorizationTests {
     @Test
     public void test02LoginSuccess() {
         open("https://slqamsk.github.io/cases/slflights/v01/");
-        $(By.id("username")).sendKeys("standard1_user");
-        $(By.id("password")).type("stand1_pass1");
+        $(By.id("username")).sendKeys("stand_pass1");
+        $(By.id("password")).type("standard_user");
         $(By.id("loginButton")).click();
-        $(By.id("message")).shouldHave(text("Неверное имя"));
+        $(By.id("message")).shouldHave(text("Неверное имя пользователя"));
 
     }
 }
