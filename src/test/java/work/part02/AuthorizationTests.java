648ace924fe1583f1e5c21ab93434b98f0187661
bytyavka <natgenyur@mail.ru>
2025-12-07 15:43:36 +0300

    DZ3

---
+++ src/test/java/work/part02/AuthorizationTests.java
@@ -0,0 +1,28 @@
+package work.part02;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class AuthorizationTests {
+    @Test
+    public void test01LoginSuccess() {
+        open("https://slqamsk.github.io/cases/slflights/v01/");
+        $(By.id("username")).sendKeys("standard_user");
+        $(By.id("password")).type("stand_pass1");
+        $(By.id("loginButton")).click();
+        $(By.id("greeting")).shouldHave(text("Добро пожаловать,"));
+
+    }
+    @Test
+    public void test02LoginSuccess() {
+        open("https://slqamsk.github.io/cases/slflights/v01/");
+        $(By.id("username")).sendKeys("standard1_user");
+        $(By.id("password")).type("stand1_pass1");
+        $(By.id("loginButton")).click();
+        $(By.id("message")).shouldHave(text("Неверное имя"));
+
+    }
+}
